b23590e0d55e9c697a05953134b0744641d2a027
Sujal Pamrar <parmarsujal098@gmail.com>
2025-03-09 22:44:29 +0530

    updates

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -21,12 +21,10 @@ const About = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      {/* CONTAINER */}
-      <div className="max-w-[700px] mx-auto text-center pt-12 ">
-        <div className="p-4 flex flex-col gap-20 lg:gap-32 xl:gap-52 ">
+      <div className="max-w-[700px] mx-auto text-center pt-12">
+        <div className="p-4 flex flex-col gap-20 lg:gap-32 xl:gap-52">
           {/* BIOGRAPHY */}
           <div className="flex flex-col justify-center items-center gap-12 xl:gap-10">
-            {/* IMAGE */}
             <Image
               src="/myImage.jpg"
               alt="Profile Picture"
@@ -34,233 +32,77 @@ const About = () => {
               width={100}
               className="rounded-full h-40 w-40 object-cover object-center"
             />
-            {/* TITLE */}
             <h1 className="font-bold text-2xl">BIOGRAPHY</h1>
-            {/* DESCRIPTION */}
             <p className="text-xl px-4">
-              As a fullstack developer and UI developer, I'm dedicated to
+              As a fullstack developer and UI developer, I&apos;m dedicated to
               creating seamless digital experiences. I focus on user-centric
               design principles to craft elegant interfaces that blend
               aesthetics with functionality. My passion lies in shaping the
               digital landscape with purpose and innovation.
             </p>
-            {/* QUOTE */}
             <span className="italic">
               Lorem ipsum dolor sit amet consectetur adipisicing.
             </span>
-            {/* SCROLL SVG */}
-            <motion.svg
-            initial={{opacity:0.2,y:0}}
-            animate={{opacity:1,y:"10px"}}
-            transition={{repeat:Infinity,duration:3,ease:"easeInOut"}}
-              width="70px"
-              height="50px"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M19 15C19 18.866 15.866 22 12 22C8.13401 22 5 18.866 5 15V9C5 5.13401 8.13401 2 12 2C15.866 2 19 5.13401 19 9V11"
-                stroke="black"
-                strokeWidth="1.5"
-                strokeLinecap="round"
-              />
-              <path
-                d="M10.5 8.5C10.5 7.67157 11.1716 7 12 7C12.8284 7 13.5 7.67157 13.5 8.5V10.5C13.5 11.3284 12.8284 12 12 12C11.1716 12 10.5 11.3284 10.5 10.5V8.5Z"
-                stroke="#1C274C"
-                strokeWidth="1.5"
-              />
-              <path
-                d="M12 2V7"
-                stroke="black"
-                strokeWidth="1.5"
-                strokeLinecap="round"
-              />
-            </motion.svg>
           </div>
 
           {/* SKILLS */}
           <div className="flex flex-col gap-24 justify-center items-center">
             <h1 className="font-bold text-2xl">SKILLS</h1>
             <div className="flex gap-6 flex-wrap justify-center">
-              {[
-                "HTML",
-                "CSS",
-                "Javascript",
-                "React.js",
-                "Next Js",
-                "Node.js",
-                "Tailwind CSS",
-                "Bootstrap",
-                "Express.js",
-                "MongoDB",
-                "SQL",
-                "Framer Motion",
-                "GSAP",
-                "Redux",
-                "WebGL",
-                "Git",
-                "Github",
-                "Figma",
-                "Vs code",
-                "Postman",
-              ].map((skill) => (
+              {["HTML", "CSS", "Javascript", "React.js", "Next.js", "Node.js", "Tailwind CSS", "Bootstrap", "Express.js", "MongoDB", "SQL", "Framer Motion", "GSAP", "Redux", "WebGL", "Git", "Github", "Figma", "VS Code", "Postman"].map((skill) => (
                 <motion.div
+                  key={skill}
                   variants={fadeInAnimation}
                   initial="initial"
                   whileInView="animate"
-                  viewport={true}
-                  key={skill}
                   className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black"
                 >
                   {skill}
                 </motion.div>
               ))}
             </div>
-            {/* SCROLL SVG */}
-            <motion.svg
-            initial={{opacity:0.2,y:0}}
-            animate={{opacity:1,y:"10px"}}
-            transition={{repeat:Infinity,duration:3,ease:"easeInOut"}}
-              width="70px"
-              height="50px"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M19 15C19 18.866 15.866 22 12 22C8.13401 22 5 18.866 5 15V9C5 5.13401 8.13401 2 12 2C15.866 2 19 5.13401 19 9V11"
-                stroke="black"
-                strokeWidth="1.5"
-                strokeLinecap="round"
-              />
-              <path
-                d="M10.5 8.5C10.5 7.67157 11.1716 7 12 7C12.8284 7 13.5 7.67157 13.5 8.5V10.5C13.5 11.3284 12.8284 12 12 12C11.1716 12 10.5 11.3284 10.5 10.5V8.5Z"
-                stroke="#1C274C"
-                strokeWidth="1.5"
-              />
-              <path
-                d="M12 2V7"
-                stroke="black"
-                strokeWidth="1.5"
-                strokeLinecap="round"
-              />
-            </motion.svg>
           </div>
 
           {/* EXPERIENCE */}
           <div className="flex flex-col justify-center pb-28">
-            {/* ECPERIENCE TITLE */}
             <h1 className="font-bold text-2xl pb-20">EXPERIENCE</h1>
-
-            {/* EXPERIENCE LIST */}
-            <div className="">
-              {/* LIST ITEM */}
-              <div className="flex justify-between h-48 ">
-                {/* LEFT */}
-                <div className="w-1/3">
-                  {/* TRAINING */}
-                  <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
-                    Geeks for geeks
+            {[...Array(3)].map((_, index) => (
+              <div key={index} className="flex justify-between h-48">
+                {index % 2 === 0 ? (
+                  <div className="w-1/3">
+                    <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
+                      Geeks for Geeks
+                    </div>
+                    <div className="p-3 text-sm italic">
+                      I have done training in Geeks for Geeks for a full-stack developer.
+                    </div>
+                    <div className="p-3 text-red-400 text-sm font-semibold">2025 - Present</div>
+                    <div className="p-1 rounded bg-white text-sm font-semibold w-fit">GFG</div>
                   </div>
-                  {/* DESCRIPTION */}
-                  <div className="p-3 text-sm italic">
-                    I have done training in Geek for Geeks for a full stack
-                    developer
-                  </div>
-                  {/* DATE */}
-                  <div className="p-3 text-red-400 text-sm font-semibold">
-                    2025 - present
-                  </div>
-                  {/* COMPANY */}
-                  <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
-                    GFG
-                  </div>
-                </div>
-                {/* CENTER */}
+                ) : (
+                  <div className="w-1/3"></div>
+                )}
                 <div className="w-1/6 flex justify-center">
-                  {/* LINE */}
                   <div className="h-full w-1 bg-gray-600 rounded relative">
-                    {/* CIRCLE */}
                     <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
                   </div>
                 </div>
-                {/* RIGHT */}
-                <div className=" w-1/3"></div>
+                {index % 2 !== 0 ? (
+                  <div className="w-1/3">
+                    <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
+                      Geeks for Geeks
+                    </div>
+                    <div className="p-3 text-sm italic">
+                      I have done training in Geeks for Geeks for a full-stack developer.
+                    </div>
+                    <div className="p-3 text-red-400 text-sm font-semibold">2025 - Present</div>
+                    <div className="p-1 rounded bg-white text-sm font-semibold w-fit">GFG</div>
+                  </div>
+                ) : (
+                  <div className="w-1/3"></div>
+                )}
               </div>
-            </div>
-
-            <div className="">
-              {/* LIST ITEM */}
-              <div className="flex justify-between h-48 ">
-                {/* LEFT */}
-                <div className="w-1/3"></div>
-                {/* CENTER */}
-                <div className="w-1/6 flex justify-center">
-                  {/* LINE */}
-                  <div className="h-full w-1 bg-gray-600 rounded relative">
-                    {/* CIRCLE */}
-                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
-                  </div>
-                </div>
-                {/* RIGHT */}
-                <div className=" w-1/3">
-                  {/* TRAINING */}
-                  <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
-                    Geeks for geeks
-                  </div>
-                  {/* DESCRIPTION */}
-                  <div className="p-3 text-sm italic">
-                    I have done training in Geek for Geeks for a full stack
-                    developer
-                  </div>
-                  {/* DATE */}
-                  <div className="p-3 text-red-400 text-sm font-semibold">
-                    2025 - present
-                  </div>
-                  {/* COMPANY */}
-                  <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
-                    GFG
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="">
-              {/* LIST ITEM */}
-              <div className="flex justify-between h-48 ">
-                {/* LEFT */}
-                <div className="w-1/3">
-                  {/* TRAINING */}
-                  <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
-                    Geeks for geeks
-                  </div>
-                  {/* DESCRIPTION */}
-                  <div className="p-3 text-sm italic">
-                    I have done training in Geek for Geeks for a full stack
-                    developer
-                  </div>
-                  {/* DATE */}
-                  <div className="p-3 text-red-400 text-sm font-semibold">
-                    2025 - present
-                  </div>
-                  {/* COMPANY */}
-                  <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
-                    GFG
-                  </div>
-                </div>
-                {/* CENTER */}
-                <div className="w-1/6 flex justify-center">
-                  {/* LINE */}
-                  <div className="h-full w-1 bg-gray-600 rounded relative">
-                    {/* CIRCLE */}
-                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
-                  </div>
-                </div>
-                {/* RIGHT */}
-                <div className=" w-1/3"></div>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,49 +1,69 @@
 "use client";
 import Image from "next/image";
 import { motion } from "framer-motion";
+
 const Homepage = () => {
   return (
-    <motion.div className="h-full" initial={{y:"-200vh"}} animate={{y:"0%"}} transition={{duration:1}} >
+    <motion.div
+      className="h-full"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+    >
       <div className="w-11/12 max-w-3xl text-center mx-auto h-full flex flex-col items-center justify-center gap-4 leading-8 overflow-x-hidden">
+        {/* Profile Image */}
         <div>
           <Image
             src="/myImage.jpg"
-            alt="image"
+            alt="Profile Image"
             height={100}
             width={100}
             className="rounded-full h-40 w-40 object-cover object-center"
           />
         </div>
-        <h3 className="text-center text-xl md:text-2xl mb-3 flex gap-2 font-semibold ">
-          Hi! I'm Sujal Parmar{" "}
-          <Image src="/hand-icon.png" alt="image" height={20} width={25} />
+
+        {/* Introduction */}
+        <h3 className="text-xl md:text-2xl mb-3 flex gap-2 font-semibold">
+          Hi! I am Sujal Parmar
+          <Image src="/hand-icon.png" alt="Hand Icon" height={20} width={25} />
         </h3>
-        <h1 className="text-3xl sm:text-6xl lg:text-[66px] font-bold ">
-          Full stack web developer
+
+        {/* Title */}
+        <h1 className="text-3xl sm:text-6xl lg:text-[66px] font-bold">
+          Full Stack Web Developer
         </h1>
+
+        {/* Description */}
         <p className="max-w-2xl mx-auto">
-          I am a ful stack web developer from Uttarakhand, India with 1 year of
-          experience in multiple companies like ..............
+          I am a full stack web developer from Uttarakhand, India, with 1 year
+          of experience in multiple companies like ..............
         </p>
+
+        {/* Buttons */}
         <div className="flex flex-col sm:flex-row items-center gap-4 mt-4">
           <a
             href="/contact"
-            className="px-10 py-3 bg-black text-white rounded-[10px] "
+            className="px-10 py-3 bg-black text-white rounded-[10px] hover:bg-gray-900 transition"
           >
             Contact Me
           </a>
+
           <a
             href="/Resume.pdf"
             download
-            className="px-7 py-3 border rounded-[10px] border-gray-500 flex items-center gap-2 "
+            className="px-7 py-3 border rounded-[10px] border-gray-500 flex items-center gap-2 hover:bg-gray-100 transition"
           >
-            My resume <Image src="/download-icon.png" alt="image" width={17} height={24} />{" "}
+            My Resume
+            <Image
+              src="/download-icon.png"
+              alt="Download Icon"
+              width={17}
+              height={24}
+            />
           </a>
         </div>
       </div>
     </motion.div>
-    
-    
   );
 };
 
